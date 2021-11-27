e639158a94f029f2df0ec250a4b865af4ed4f203
Daniel Kim <51827691+folaroma@users.noreply.github.com>
2021-11-26 19:34:24 -0500

    finished up header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ChakraProvider } from "@chakra-ui/react";
+import Home from "./components/Home/";
+import Header from "./components/Header/";
+import About from "./components/About/";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+      <ChakraProvider>
+        <BrowserRouter>
+          <Header/>
+          <Routes>
+            <Route path="/" element={<Home />} /> 
+            <Route path="/about" element={<About />} />
+          </Routes> 
+        </BrowserRouter>
+      </ChakraProvider>
+
+    );
 }
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Flex, Box, Link, Spacer, Center, Heading } from "@chakra-ui/react";
+import {Link as ReachLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+      <>
+        <Flex p={"5em"}>
+          <Box ms={"5em"}>
+            <Link as={ ReachLink } to="/art" onClick="window.location.reload();" fontWeight="bold" fontSize="md" pb={1} _hover={{color: "#f40"}} borderBottomColor={ window.location.pathname === "/art" ? "#f40" :null} borderBottomWidth={ window.location.pathname === "/art" ? "0.3em" :null} color={ window.location.pathname === "/art" ? "#f40" :null}>ART</Link>
+            <Link as={ ReachLink } to="/about" onClick="window.location.reload();" fontWeight="bold" fontSize="md" ms={10} pb={1} _hover={{color: "#f40"}} borderBottomColor={ window.location.pathname === "/about" ? "#f40" :null} borderBottomWidth={ window.location.pathname === "/about" ? "0.3em" :null} color={ window.location.pathname === "/about" ? "#f40" :null}>ABOUT</Link>
+          </Box>
+          <Spacer />
+          <Center mt="-0.75em">
+            <Heading _hover={{color: "#f40"}}>apocalypticism</Heading>
+          </Center>
+          <Spacer />
+          <Box me={"5em"}>
+            <Link as={ ReachLink } to="/daniel" onClick="window.location.reload();" fontWeight="bold" fontSize="md" pb={1} _hover={{color: "#f40"}} borderBottomColor={ window.location.pathname === "/daniel" ? "#f40" :null} borderBottomWidth={ window.location.pathname === "/daniel" ? "0.3em" :null} color={ window.location.pathname === "/daniel" ? "#f40" :null}>DANIEL KIM</Link>
+            <Link as={ ReachLink } to="/david" onClick="window.location.reload();" fontWeight="bold" fontSize="md" ms={10} pb={1} _hover={{color: "#f40"}} borderBottomColor={ window.location.pathname === "/david" ? "#f40" :null} borderBottomWidth={ window.location.pathname === "/david" ? "0.3em" :null} color={ window.location.pathname === "/david" ? "#f40" :null}>DAVID HUH</Link>
+          </Box>
+        </Flex>
+      </>
+  );
+};
+
+export default Header;
