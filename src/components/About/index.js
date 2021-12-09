5629271a3c748265fce996c90be95efa14c1ba17
folaroma <dkim1106@gmail.com>
2021-12-09 18:54:45 -0500

    fixing up text boxes

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,6 +7,12 @@ const About = () => {
       <Flex p={"5em"}>
         <Box ms={"5em"}>
           <Heading fontWeight="bold" size="4xl">PHIL1260</Heading>
+          <Text fontSize="lg" mt={"3em"} w={"50vw"} lineHeight={"200%"}>
+            <span style={{color: "#f40"}}>Apocalypticism in Film</span>, or PHIL1260, is an introductory-interdisciplinary course in Religious Studies and Film at Northeastern University, which examines the diverse and changing presentation of apocalypticism in film.
+            This class is run by <span style={{color: "#f40"}}>Professor Shiela F. Winborne</span>, who is a faculty member on the Religious Studies side of the Philosophy and Religion department, and was run entirely asynchronously online due to COVID-19.
+            This website was created for the <span style={{color: "#f40"}}>Creative Final Project for this course</span>, and consists of various forms of apocalypticism in media, ranging from film to art from different eras in history.
+            Both members of this project, <span style={{color: "#f40"}}>Daniel</span> and <span style={{color: "#f40"}}>David</span>, are Computer Science majors at Northeastern, and wished to create something relating to both their major, and this class, and thus, this website was developed.
+          </Text>
         </Box>
       </Flex>
       </>
--- src/components/David/index.js
+++ src/components/David/index.js
@@ -8,6 +8,9 @@ const David = () => {
         <Box ms={"5em"}>
           <Heading fontWeight="bold" size="4xl">About Me</Heading>
           <Text fontSize="lg" mt={"3em"} w={"50vw"} lineHeight={"200%"}>
+          Hello! My name's <span style={{color: "#f40"}}>David Huh</span> and I'm also a second year Computer Science student at Northeastern University! 
+          Like my partner, because of my major in CS, I was interested in doing a project that would allow me utilize my skills from CS and interests in this course. 
+          Thus, we ended up creating this website! I hope you enjoy!
           </Text>
         </Box>
       </Flex>
