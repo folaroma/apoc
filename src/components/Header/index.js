04b1d78e05865c0b3c05347d40c0bd5d51b1d01e
folaroma <dkim1106@gmail.com>
2021-12-09 18:41:20 -0500

    fixing more router stuff

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -13,9 +13,9 @@ const Header = () => {
       <>
         <Flex p={"5em"}>
           <Box ms={"5em"}>
-            <Link as={ ReachLink } to="/" onClick="window.location.reload();" fontWeight="bold" fontSize="md" pb={1} _hover={{color: "#f40"}} borderBottomColor={ window.location.pathname === "/" ? "#f40" :null} borderBottomWidth={ window.location.pathname === "/" ? "0.3em" :null} color={ window.location.pathname === "/" ? "#f40" :null}>ART</Link>
-            <Link as={ ReachLink } to="/about" onClick="window.location.reload();" fontWeight="bold" fontSize="md" ms={10} pb={1} _hover={{color: "#f40"}} borderBottomColor={ window.location.pathname === "/about" ? "#f40" :null} borderBottomWidth={ window.location.pathname === "/about" ? "0.3em" :null} color={ window.location.pathname === "/about" ? "#f40" :null}>ABOUT</Link>
-            <Link as={ ReachLink } to="/cite" onClick="window.location.reload();" fontWeight="bold" fontSize="md" ms={10} pb={1} _hover={{color: "#f40"}} borderBottomColor={ window.location.pathname === "/cite" ? "#f40" :null} borderBottomWidth={ window.location.pathname === "/cite" ? "0.3em" :null} color={ window.location.pathname === "/cite" ? "#f40" :null}>CITATION</Link>
+            <Link as={ ReachLink } to="/" fontWeight="bold" fontSize="md" pb={1} _hover={{color: "#f40"}} borderBottomColor={ window.location.pathname === "/" ? "#f40" :null} borderBottomWidth={ window.location.pathname === "/" ? "0.3em" :null} color={ window.location.pathname === "/" ? "#f40" :null}>ART</Link>
+            <Link as={ ReachLink } to="/about" fontWeight="bold" fontSize="md" ms={10} pb={1} _hover={{color: "#f40"}} borderBottomColor={ window.location.pathname === "/about" ? "#f40" :null} borderBottomWidth={ window.location.pathname === "/about" ? "0.3em" :null} color={ window.location.pathname === "/about" ? "#f40" :null}>ABOUT</Link>
+            <Link as={ ReachLink } to="/cite" fontWeight="bold" fontSize="md" ms={10} pb={1} _hover={{color: "#f40"}} borderBottomColor={ window.location.pathname === "/cite" ? "#f40" :null} borderBottomWidth={ window.location.pathname === "/cite" ? "0.3em" :null} color={ window.location.pathname === "/cite" ? "#f40" :null}>CITATION</Link>
           </Box>
           <Spacer />
           <Center mt="-0.75em">
@@ -23,8 +23,8 @@ const Header = () => {
           </Center>
           <Spacer />
           <Box me={"5em"}>
-            <Link as={ ReachLink } to="/daniel" onClick="window.location.reload();" fontWeight="bold" fontSize="md" pb={1} _hover={{color: "#f40"}} borderBottomColor={ window.location.pathname === "/daniel" ? "#f40" :null} borderBottomWidth={ window.location.pathname === "/daniel" ? "0.3em" :null} color={ window.location.pathname === "/daniel" ? "#f40" :null}>DANIEL KIM</Link>
-            <Link as={ ReachLink } to="/david" onClick="window.location.reload();" fontWeight="bold" fontSize="md" ms={10} pb={1} _hover={{color: "#f40"}} borderBottomColor={ window.location.pathname === "/david" ? "#f40" :null} borderBottomWidth={ window.location.pathname === "/david" ? "0.3em" :null} color={ window.location.pathname === "/david" ? "#f40" :null}>DAVID HUH</Link>
+            <Link as={ ReachLink } to="/daniel" fontWeight="bold" fontSize="md" pb={1} _hover={{color: "#f40"}} borderBottomColor={ window.location.pathname === "/daniel" ? "#f40" :null} borderBottomWidth={ window.location.pathname === "/daniel" ? "0.3em" :null} color={ window.location.pathname === "/daniel" ? "#f40" :null}>DANIEL KIM</Link>
+            <Link as={ ReachLink } to="/david" fontWeight="bold" fontSize="md" ms={10} pb={1} _hover={{color: "#f40"}} borderBottomColor={ window.location.pathname === "/david" ? "#f40" :null} borderBottomWidth={ window.location.pathname === "/david" ? "0.3em" :null} color={ window.location.pathname === "/david" ? "#f40" :null}>DAVID HUH</Link>
           </Box>
         </Flex>
       </>
